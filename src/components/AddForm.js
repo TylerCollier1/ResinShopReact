1ade30b299c900a4792ca1f7f5212c923db7d643
Tyler Collier <tcollier@dev-10.com>
2022-05-11 13:11:27 -0500

    ArtSlice of React

---
+++ src/components/AddForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+const AddForm = () => {
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+
+        let art = {
+            Height: document.getElementById("Height").value,
+            Width: document.getElementById("Width").value,
+            MaterialQuantity: document.getElementById("MaterialQuantity").value,
+            ColorQuantity: document.getElementById("ColorQuantity").value,
+            Cost: document.getElementById("Cost").value
+        }
+
+        const url = "https://localhost:7143/api/art";
+
+        const init = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(art)
+        };
+    
+        fetch(url, init).
+        then(response=>{
+            if(response.status !== 201){
+                alert("Error: status " + response.status);
+                return Promise.reject(response.status);
+            }
+            return response.json();
+        })
+        .then(json=>{
+            alert("successfully added new art with ID of " + json.artId)
+        })
+    }
+
+  return (
+    <>
+    <br/>
+    <h3>ADD</h3>
+    <form name = "add-form" onSubmit = {handleSubmit}>
+      <label>Height (inches): </label>
+      <input type="text" name="Height" id="Height"/><br></br>
+      <label>Width (inches): </label>
+      <input type="text" name="Width" id="Width"/><br></br>
+      <label>MaterialQuantity: </label>
+      <input type = "number" id = "MaterialQuantity" /> <br></br>
+      <label>ColorQuantity: </label>
+      <input type="number" step="1" id="ColorQuantity" /> <br></br>
+      <label>Cost: </label>
+      <input type="number" step="0.01" id="Cost"/><br></br>
+      <input type="submit" value="Add Art" />
+    </form>
+    </>
+  )
+}
+
+export default AddForm
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div name = "hd">
+    <h1> Resin Shop </h1>
+    </div>
+  )
+}
+
+export default Header
