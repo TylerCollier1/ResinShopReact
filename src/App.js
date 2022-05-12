9bb087ea0300eed564749d49c77461291d913eeb
Lindsey Bremer <Lbremer@dev-10.com>
2022-05-12 15:34:38 -0500

    Added routing and order functions(WIP)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import HomePage2 from './components2/HomePage2';
+import OrderHomePage from './ordercomponents/OrderHomePage';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HomePage />
-      <HomePage2 />
-    </div>
+    <Router>
+      <nav>
+        <Link to='/'> Home </Link>
+        <Link to='/art'> Art </Link>
+        <Link to='/customer'> Customer </Link>
+        <Link to='/order'> Order </Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Header />} />        
+        <Route path='/art' element={<HomePage />} />
+        <Route path='/customer' element={<HomePage2 />} />
+        <Route path='/order' element={<OrderHomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
